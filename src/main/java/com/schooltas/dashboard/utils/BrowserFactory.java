d1c6afe6db9a943c11dbd4eb3919cfb9d6ef0e61
Antal, Dana <danaantal@gmail.com>
2017-12-20 08:22:58 +0200

    Added both paths for chromedriver.

--- src/main/java/com/schooltas/dashboard/utils/BrowserFactory.java
+++ src/main/java/com/schooltas/dashboard/utils/BrowserFactory.java
@@ -8,23 +8,23 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class BrowserFactory {
 
 	static WebDriver driver;
-	
+
 	public static WebDriver startBrowser(String browserName, String url){
 		if(browserName.equalsIgnoreCase("firefox")){
 			driver = new FirefoxDriver();
 		}
-		
+
 		else if(browserName.equalsIgnoreCase("chrome")){
 			driver = new ChromeDriver();
 		}
-		
+
 		else if(browserName.equalsIgnoreCase("IE")){
 			driver = new InternetExplorerDriver();
 		}
-		
+
 		driver.manage().window().maximize();
 		driver.get(url);
-		
+
 		return driver;
 	}
 }

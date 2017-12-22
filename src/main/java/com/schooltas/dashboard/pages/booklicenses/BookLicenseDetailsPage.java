f066f174b748260170fb6299178b340629cb03e4
Antal, Dana <danaantal@gmail.com>
2017-12-22 13:34:27 +0200

    Added methods to assert license details after creation.

---
+++ src/main/java/com/schooltas/dashboard/pages/booklicenses/BookLicenseDetailsPage.java
@@ -0,0 +1,62 @@
+package com.schooltas.dashboard.pages.booklicenses;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.internal.collections.Pair;
+
+public class BookLicenseDetailsPage {
+
+	private final WebDriver driver;
+
+	public BookLicenseDetailsPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	@FindBy(how = How.CLASS_NAME, using = "dl-horizontal")
+	WebElement licenseDetails;
+
+
+	public Map<String, String> createBookLicenseDetailsMap(){
+
+		Map<String, String> bookLicenseDetails = new HashMap<>();
+
+		List<WebElement> childs = licenseDetails.findElements(By.xpath(".//*"));
+
+		String detailName = null;
+		for(WebElement element : childs){
+			if(element.getTagName().equals("dt")){
+				detailName = element.getText();
+			}
+			else{
+				if(element.getTagName().equals("dd")){
+					bookLicenseDetails.put(detailName, element.getText());
+				}
+			}
+		}
+		return bookLicenseDetails;
+	}
+
+	public void assertBookLicenseDetails() {
+		CreateBookLicensePage createLicense = PageFactory.initElements(driver, CreateBookLicensePage.class);
+
+		ArrayList<Pair<WebElement, String>> licenseDetailsList = createLicense.createLicenseDetailsInputList();
+		Map<String, String> licenseDetailsMap = createBookLicenseDetailsMap();
+
+		for(Pair<WebElement, String> licenseDetail : licenseDetailsList){
+			String elementName = licenseDetail.first().getText();
+			assertEquals(licenseDetail.second(), licenseDetailsMap.get(elementName));
+
+		}
+	}
+}
--- src/main/java/com/schooltas/dashboard/pages/booklicenses/CreateBookLicensePage.java
+++ src/main/java/com/schooltas/dashboard/pages/booklicenses/CreateBookLicensePage.java
@@ -1,16 +1,19 @@
 package com.schooltas.dashboard.pages.booklicenses;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.internal.collections.Pair;
 
 public class CreateBookLicensePage {
 
 	private final WebDriver driver;
 
-	public CreateBookLicensePage(WebDriver driver){
+	public CreateBookLicensePage(WebDriver driver) {
 		this.driver = driver;
 	}
 
@@ -30,14 +33,35 @@ public class CreateBookLicensePage {
 	WebElement saveBtn;
 
 
-	public void createBookLicense(){
+
+	public void createBookLicense() {
 
 		AddBookToLicensePage addBook = PageFactory.initElements(driver, AddBookToLicensePage.class);
 		addBook.addBookToLicense();
 
-		licenseNameField.sendKeys("test license automate");
-		licenseMaxUsersField.sendKeys("10");
-		licenseDurationCmbBox.sendKeys("365");
+		ArrayList<Pair<WebElement, String>> licenseDetailsList = createLicenseDetailsInputList();
+
+		licenseDetailsList.forEach((element) -> {
+			if(!element.first().getText().equals(null)){
+				element.first().clear();
+				element.first().sendKeys(element.second());
+			}
+			else{
+				element.first().sendKeys(element.second());
+			}
+		});
+
 		saveBtn.click();
+	}
+
+	public ArrayList<Pair<WebElement, String>> createLicenseDetailsInputList(){
+
+		ArrayList<Pair<WebElement, String>> createLicenseFormPairs = new ArrayList<>();
+
+		createLicenseFormPairs.add(new Pair<>(licenseNameField, "license title automate"));
+		createLicenseFormPairs.add(new Pair<>(licenseMaxUsersField, "10"));
+		createLicenseFormPairs.add(new Pair<>(licenseDurationCmbBox, "365"));
+
+		return createLicenseFormPairs;
 	}
 }
